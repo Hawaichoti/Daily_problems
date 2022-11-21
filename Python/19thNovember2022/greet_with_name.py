52985a2d12fda853dba5e55ee572077e1f04b3b6
Sohinee Mondal <40481848+Sohinee-nimo@users.noreply.github.com>
2022-11-22 00:10:51 +0530

    Create greet_with_name.py
    a program that asks the user for their name and greets them with their name.

---
+++ Python/19thNovember2022/greet_with_name.py
@@ -0,0 +1,4 @@
+#Write a program that asks the user for their name and greets them with their name.
+a = input("Input your name ")#input function takes input from user. Input is always in string.
+print("Hello! " + a)#The print() function prints the specified message to the screen, or other standard output device.
+#The message can be a string, or any other object, the object will be converted into a string before written to the screen.ame.
